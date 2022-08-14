1027a14f88d48dcbe36f9d2678b162588eb30633
Drew Miller <49833875+wwdrew@users.noreply.github.com>
2022-08-14 19:20:05 +0100

    fix: put back removed preset

--- example/babel.config.js
+++ example/babel.config.js
@@ -2,8 +2,8 @@ const path = require('path');
 const pak = require('../package.json');
 
 module.exports = {
+  presets: ['module:metro-react-native-babel-preset'],
   plugins: [
-    ["module:react-native-dotenv"],
     [
       'module-resolver',
       {
